85b629f4e1fcffda0b119d7eea54887258578649
likemoses2025 <mose233@gmail.com>
2023-08-08 15:39:06 +0900

    First Commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello World</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
