748574c398e25d4a907fcccd4f19b3fb4aec9408
likemoses2025 <mose233@gmail.com>
2023-08-11 16:20:08 +0900

    Create Search Component

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
 import globalStyles from '../../assets/styles/globalStyle';
-import Header from '../../components/Header/Header';
-import Tab from '../../components/Tab/Tab';
-import Badge from '../../components/Badge/Badge';
-import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import {faSearch} from '@fortawesome/free-solid-svg-icons';
+import Search from '../../components/Search/Search';
 
 const Home = () => {
   return (
     <SafeAreaView style={[globalStyles.backgroundWhite, globalStyles.flex]}>
-      <Header title={'Azzahri. A.'} type={1} />
-
-      <Badge title={'Badge'} isInactive={true} />
-      <FontAwesomeIcon icon={faSearch} />
+      <Search
+        onSearch={value => {
+          console.log(value);
+        }}
+      />
     </SafeAreaView>
   );
 };
