d9a3b8adce6963570b8a3f71ae1e41a4ed032044
likemoses2025 <mose233@gmail.com>
2023-08-15 23:34:29 +0900

    Persist AsyncStorage

--- redux/store.js
+++ redux/store.js
@@ -1,17 +1,36 @@
-import {combineReducers} from 'redux';
-import {configureStore, getDefaultMiddleware} from '@reduxjs/toolkit';
+import {
+  configureStore,
+  combineReducers,
+  getDefaultMiddleware,
+} from '@reduxjs/toolkit';
+
+// redux 데이터 유지 , asyncstorage(비암호화,전역 적용 저장 시스템)
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {persistReducer, persistStore} from 'redux-persist';
+
+// 로거셋팅
 import {logger} from 'redux-logger';
+
 import User from './reducers/User';
 
 const rootReducer = combineReducers({
   user: User,
 });
 
+// AsyncStorage Setting
+const configuration = {
+  key: 'root',
+  storage: AsyncStorage,
+  version: 1,
+};
+const persistedReducer = persistReducer(configuration, rootReducer);
+
 const store = configureStore({
-  reducer: rootReducer,
-  //   middleware: getDefaultMiddleware => {
-  //     return getDefaultMiddleware().concat(logger);
-  //   },
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware => {
+    return getDefaultMiddleware({serializableCheck: false}).concat(logger);
+  },
 });
 
 export default store;
+export const persistor = persistStore(store);
