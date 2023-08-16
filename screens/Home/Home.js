76cadd95b693ab3ad7d5a99095cf7a15157b100e
likemoses2025 <mose233@gmail.com>
2023-08-16 22:57:34 +0900

    Building Home Screen

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,21 +1,108 @@
-/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {Pressable, SafeAreaView, Text} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  ScrollView,
+  Image,
+  Pressable,
+} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
+
 import globalStyles from '../../assets/styles/globalStyle';
+import style from './style';
 import Header from '../../components/Header/Header';
-import {updateFirstName} from '../../redux/reducers/User';
+import Search from '../../components/Search/Search';
+import {resetToInitialState} from '../../redux/reducers/User';
 
 const Home = () => {
   const user = useSelector(state => state.user);
   const dispatch = useDispatch();
-  console.log(user);
+
+  const categories = [
+    {
+      categoryId: 1,
+      name: 'Highlight',
+    },
+    {
+      categoryId: 2,
+      name: 'Environment',
+    },
+    {
+      categoryId: 3,
+      name: 'Education',
+    },
+    {
+      categoryId: 4,
+      name: 'Clothing and Accessories',
+    },
+    {
+      categoryId: 5,
+      name: 'Household goods',
+    },
+    {
+      categoryId: 6,
+      name: 'Electronics',
+    },
+    {
+      categoryId: 7,
+      name: 'Toys and Games',
+    },
+    {
+      categoryId: 8,
+      name: 'Sports Equipment',
+    },
+    {
+      categoryId: 9,
+      name: 'Books and Media',
+    },
+    {
+      categoryId: 10,
+      name: 'Health and Beauty Products',
+    },
+    {
+      categoryId: 11,
+      name: 'Office supplies',
+    },
+    {
+      categoryId: 12,
+      name: 'Tools and Hardware',
+    },
+    {
+      categoryId: 13,
+      name: 'Art and Craft Supplies',
+    },
+  ];
+
   return (
     <SafeAreaView style={[globalStyles.backgroundWhite, globalStyles.flex]}>
-      <Header title={user.firstName + user.lastName} />
-      <Pressable onPress={() => dispatch(updateFirstName({firstName: 'J'}))}>
-        <Text>Press me to change first name</Text>
-      </Pressable>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={style.header}>
+          <View>
+            <Text style={style.headerIntroText}>Hello, </Text>
+            <View style={style.username}>
+              <Header
+                title={user.firstName + ' ' + user.lastName[0] + '. 👋'}
+              />
+            </View>
+          </View>
+          <Image
+            source={{uri: user.profileImage}}
+            style={style.profileImage}
+            resizeMode={'contain'}
+          />
+        </View>
+        <View style={style.searchBox}>
+          <Search />
+        </View>
+        <Pressable style={style.highlightedImageContainer}>
+          <Image
+            style={style.highlightedImage}
+            source={require('../../assets/images/highlighted_image.png')}
+            resizeMode={'contain'}
+          />
+        </Pressable>
+      </ScrollView>
     </SafeAreaView>
   );
 };
--- screens/Home/style.js
+++ screens/Home/style.js
@@ -1,5 +1,44 @@
 import {StyleSheet} from 'react-native';
+import {
+  horizontalScale,
+  scaleFontSize,
+  verticalScale,
+} from '../../assets/styles/scaling';
 
-const style = StyleSheet.create({});
+const style = StyleSheet.create({
+  header: {
+    marginTop: verticalScale(20),
+    marginHorizontal: horizontalScale(24),
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  headerIntroText: {
+    fontFamily: 'Inter',
+    fontSize: scaleFontSize(16),
+    lineHeight: scaleFontSize(19),
+    fontWeight: '400',
+    color: '#636776',
+  },
+  username: {
+    marginTop: verticalScale(5),
+  },
+  profileImage: {
+    width: horizontalScale(50),
+    height: verticalScale(50),
+  },
+  searchBox: {
+    marginHorizontal: horizontalScale(24),
+    marginTop: verticalScale(20),
+  },
+  highlightedImageContainer: {
+    marginHorizontal: horizontalScale(24),
+    marginTop: horizontalScale(20),
+  },
+  highlightedImage: {
+    width: '100%',
+    height: 160,
+  },
+});
 
 export default style;
